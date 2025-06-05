c170b694b0bef70bedf25a0df17176ebf489449b
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2025-06-05 16:10:47 +0100

    Remove deprecated files: deleted path-parameters.md and test-expr-eval.js to streamline project structure and eliminate outdated examples.

--- test-expr-eval.js
+++
@@ -1,5 +0,0 @@
-const { Parser } = require('expr-eval');
-
-const parser = new Parser();
-const expr = parser.parse('framework=="react" ? "tsx" : "ts"');
-console.log(expr.evaluate({ framework: "react" }));
